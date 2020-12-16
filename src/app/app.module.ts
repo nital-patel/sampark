a2ff8e9d31cd4460609944863d6a3afe22092bc1
Nital Patel <patelnital36@gmail.com>
2020-12-16 13:49:27 -0500

    New project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [

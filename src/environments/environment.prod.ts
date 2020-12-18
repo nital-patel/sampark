c8b7fd810183b250afe052ee75d42a77b55a6652
Nital Patel <patelnital36@gmail.com>
2020-12-18 14:00:28 -0500

    Add firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCr9vCs1DwN3lKYQR6UAin8q4VDo_2yOOA",
+    authDomain: "sampark-app-8dd55.firebaseapp.com",
+    projectId: "sampark-app-8dd55",
+    storageBucket: "sampark-app-8dd55.appspot.com",
+    messagingSenderId: "166194748546",
+    appId: "1:166194748546:web:9867cd0f93e5563c62a89b",
+    measurementId: "G-Z6RQ4VV50T"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCr9vCs1DwN3lKYQR6UAin8q4VDo_2yOOA",
+    authDomain: "sampark-app-8dd55.firebaseapp.com",
+    projectId: "sampark-app-8dd55",
+    storageBucket: "sampark-app-8dd55.appspot.com",
+    messagingSenderId: "166194748546",
+    appId: "1:166194748546:web:9867cd0f93e5563c62a89b",
+    measurementId: "G-Z6RQ4VV50T"
+  }
 };
 
 /*

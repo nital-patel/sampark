1b2837d66aecdbec47a1fa479ef6ec7d54be15b6
Nital Patel <patelnital36@gmail.com>
2020-12-19 15:36:13 -0500

    Add rotermodule and Home-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,31 @@
+import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 
+
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderNavbarComponent } from './header-navbar/header-navbar.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderNavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+
+
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]

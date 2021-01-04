37a26f494c13228e3a3b48477255b3be7d1bab1a
Nital Patel <patelnital36@gmail.com>
2021-01-03 21:44:42 -0500

    add contacts.component.html

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'header',
+  selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
